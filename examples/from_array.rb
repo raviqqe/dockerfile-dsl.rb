5e8c0f9ff00a9ac8e697e7b601579cdd95819575
Yota Toyama <raviqqe@gmail.com>
2016-10-24 04:45:01 +0900

    Add example using block-is-array gem

---
+++ examples/from_array.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'block-is-array'
+require 'dockerfile-dsl'
+
+
+
+array = block_is_array do
+  from 'ubuntu'
+  expose 21, 22, 80, 443
+  cmd ['/bin/sh']
+end
+
+array.insert(1, [:maintainer, 'Me'])
+
+puts Dockerfile.from_array(array)
