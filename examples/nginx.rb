14b471e8d6d6fcf5edc607482aa9d4741c44493c
Yota Toyama <raviqqe@gmail.com>
2016-10-23 16:10:11 +0900

    Modify examples

--- examples/nginx.rb
+++ examples/nginx.rb
@@ -4,45 +4,11 @@ require 'dockerfile-dsl'
 
 
 
-conf = dockerfile %i(server load_module) do
-  user :nobody, :nogroup
-  worker_processes 1
-  events do
-    worker_connections 1024
-  end
-
-  http do
-    include 'mime.types'
-    default_type 'application/octet-stream'
-
-    sendfile :on
-    keepalive_timeout 65
-
-    server do
-      listen 80
-      server_name :localhost
-
-      location '/' do
-        root '/usr/local/www/nginx'
-        index 'index.html', 'index.htm'
-      end
-    end
-
-    server do
-      listen 443, :ssl
-      server_name :localhost
-
-      ssl_certificate 'cert.pem'
-      ssl_certificate_key 'key.pem'
-      ssl_session_cache 'shared:SSL:1m'
-      ssl_session_timeout '5m'
-
-      location '/' do
-        root 'html'
-        index 'index.html', 'index.htm'
-      end
-    end
-  end
+file = dockerfile do
+  from :ubuntu
+  run 'sudo apt install nginx'
+  add 'nginx.conf', '/etc/nginx.conf'
+  cmd 'service nginx start'
 end
 
-p conf
+puts file
