3977bb224dca125c8685ff07a2eebf70cba19c07
Yota Toyama <raviqqe@gmail.com>
2016-10-23 16:13:28 +0900

    Use array in example

--- examples/nginx.rb
+++ examples/nginx.rb
@@ -8,7 +8,7 @@ file = dockerfile do
   from :ubuntu
   run 'sudo apt install nginx'
   add 'nginx.conf', '/etc/nginx.conf'
-  cmd 'service nginx start'
+  cmd [:service, :nginx, :start]
 end
 
 puts file
